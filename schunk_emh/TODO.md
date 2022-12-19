eccd14c116e2a5f3cab7a7bb8c628f1a283790f3
Hauke <hauke.heeren@stud.uni-hannover.de>
2022-12-19 18:28:30 +0100

    schunk emh gripper init commit

---
+++ schunk_emh/TODO.md
@@ -0,0 +1,2 @@
+## Timing
+- Timing fürs abwarten nach greifen anpassen (hängt vermutlich von Strombegrenzung ab)
---
+++ schunk_emh/scripts/schunk_controller.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python3
+
+import rospy
+from ur_msgs.srv import SetIO
+from ur_msgs.msg import IOStates
+from std_srvs.srv import Trigger, TriggerResponse
+from std_msgs.msg import Bool, Int8
+
+from typing import Optional, List
+
+class SchunkController():
+    def __init__(self, prefix_ur="ur/", pin_magnet=16, pin_demagnet=17, pin_picked=17, pin_force_a: Optional[int]=None, pin_force_b: Optional[int]=None):
+        """To control the Schunk gripper
+
+        Args:
+            prefix_ur (str, optional): prefrix of service. Defaults to "ur/".
+            pin_magnet (int, optional): Magnetization ON. Defaults to TO0=16.
+            pin_demagnet (int, optional): Magnetization OFF. Defaults to TO1=17.
+            pin_picked (int, optional): Picked signal. Defaults to TI1=17.
+            pin_force_a (int, optional): Force signal A. Defaults to None.
+            pin_force_b (int, optional): Force signal B. Defaults to None.
+            
+        TODO Iniput:
+            Message "Workpiece available"
+            Message "Malfunction"
+            Message "Magnetization status"
+        """
+        self.prefix_ur=prefix_ur
+        self.pin_magnet=pin_magnet
+        self.pin_demagnet=pin_demagnet
+        self.pin_force_a=pin_force_a
+        self.pin_force_b=pin_force_b
+        self.pin_picked=pin_picked
+        
+        rospy.init_node('schunk_controller_node', anonymous=True)
+        self.config()
+        
+    def config(self):
+        self.srv = rospy.ServiceProxy(self.prefix_ur+'ur_hardware_interface/set_io', SetIO)
+        self.srv.wait_for_service()
+        self.set_pins()
+        
+        rospy.Service(self.prefix_ur+'schunk/grasp', Trigger, self.cb_grasp_srv)
+        rospy.Service(self.prefix_ur+'schunk/release', Trigger, self.cb_release_srv)
+        rospy.Service(self.prefix_ur+'schunk/is_picked', Trigger, self.cb_is_picked_srv)
+        
+        
+    def cb_grasp_srv(self, req: Trigger):
+        if self.grasp():
+            return TriggerResponse(True, "Grasp successfull")
+        return TriggerResponse(False, "Grasp NOT successfull")
+    
+    def cb_release_srv(self, req: Trigger):
+        if self.release():
+            return TriggerResponse(True, "Release successfull")
+        return TriggerResponse(False, "Release NOT successfull")
+    
+    def cb_is_picked_srv(self, req: Trigger):
+        if self.is_picked():
+            return TriggerResponse(True, "Object is picked")
+        return TriggerResponse(False, "Object is NOT picked")
+        
+    def listen_to_topics(self):
+        """Listen to topics to grasp and release the object
+        
+        Topics:
+            prefix_ur+"release" takes a bool
+            prefix_ur+"grasp" takes an int from 0 to 3
+        """
+        rospy.Subscriber(self.prefix_ur+"release", Bool, self.cb_release)
+        rospy.Subscriber(self.prefix_ur+"grasp", Int8, self.cb_grasp)
+        
+    def is_picked(self):
+        try:
+            msg=rospy.wait_for_message(self.prefix_ur+"ur_hardware_interface/io_states", IOStates, timeout=10)
+            return msg.digital_in_states[self.pin_picked].state 
+        except rospy.ROSException:
+            rospy.logerr("No message from IOStates")
+            return False
+    
+    def cb_release(self, msg):
+        if msg.data:
+            self.release()
+        else:
+            self.grasp()
+    
+    def cb_grasp(self, msg):
+        self.grasp(msg.data)
+        
+    def test(self):
+        rate = rospy.Rate(1)
+        while not rospy.is_shutdown():
+            print(f"Grasping: {self.grasp()}")
+            rate.sleep()
+            print(f"Releasing: {self.release()}")
+            rate.sleep()
+        print(self.release())
+        
+    def grasp(self, force: int=3):
+        """Grasps the object
+
+        Args:
+            force (int, optional): Holding force level: 3=15%, 2=25%, 1=35%, 0=100%. Defaults to 3.
+
+        Raises:
+            ValueError: if force is not 0, 1, 2, or 3
+
+        Returns:
+            bool: success
+        """
+        if not 0 <= force <= 3 or not isinstance(force, int):
+            # raise ValueError("force must be 0, 1, 2, or 3")
+            rospy.logerr("force must be 0, 1, 2, or 3")
+            return False
+        # force = bin(force)
+        # force_a = bool(int(force[-2]))
+        # force_b = bool(int(force[-1]))
+        force_a = bool(int(force/2))
+        force_b = bool(int(force%2))
+        self.set_pins([self.pin_force_a, self.pin_force_b, self.pin_demagnet], [force_a, force_b, False])
+        rospy.sleep(0.001) # wait for the signal to be set
+        self.set_pins([self.pin_magnet], [True])
+        rospy.sleep(0.006) # wait for the magnet to be set
+        self.set_pins() # reset all pins
+        rospy.sleep(0.5) # wait for the signal to be set
+        return self.is_picked() # get from io input
+        
+    def release(self):
+        self.set_pins() # reset all pins
+        self.set_pins([self.pin_demagnet], [True])
+        rospy.sleep(0.8) # wait for the signal to be set
+        return not self.is_picked() # get from io input
+    
+    def set_pins(self, pins:Optional[List[int]] = None, states:Optional[List[bool]] = None, fun=1):
+        """Set pins
+
+        Args:
+            pins (list): list of pins
+            states (list): list of states
+            fun (int, optional): function. Defaults to digital.
+        """
+        
+        if pins is None:
+            pins = [self.pin_magnet, self.pin_demagnet, self.pin_force_a, self.pin_force_b]
+            states = [False]*len(pins)
+        if states is None:
+            states = [False]*len(pins)
+        for pin, state in zip(pins, states):
+            if pin is not None and isinstance(state, bool):
+                self.srv(fun, pin, state)   #fun=1: digital out
+            else:
+                rospy.logdebug(f"pin {pin} or state {state} is not valid")
+        
+if __name__ == '__main__':
+    #Tool: Pins 16/17 are tool in/outputs. -> 16 magnet, 17 demagnet, DI17 picked 
+    gripper=SchunkController(prefix_ur="mur/ur/")
+    rospy.spin()
+    # gripper.test()
+    
