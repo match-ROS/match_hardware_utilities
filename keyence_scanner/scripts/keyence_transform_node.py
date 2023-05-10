4a3d24afd9d0c8869890624d15e073cd914a74a3
YassineMch <yassine.mechri@stu.uni-hannover.de>
2023-05-10 11:19:40 +0200

    added tcp scanner offset

--- keyence_scanner/scripts/keyence_transform_node.py
+++ keyence_scanner/scripts/keyence_transform_node.py
@@ -19,13 +19,15 @@ class KeyenceTransformNode():
     def __init__(self):
         rospy.init_node('keyence_transform_node')
 
+        self.tcp_scanner_offset = [0.0,0.0,0.7364]
+
         rospy.Subscriber("/mur620c/UR10_r/ur_calibrated_pose",PoseStamped, self.pose_cb)   
         rospy.sleep(1)
         rospy.Subscriber("/profiles",PointCloud2, self.pointcloud_cb)   
         self.cloud_pub = rospy.Publisher("/cloud_out",PointCloud2, queue_size = 10)
         self.cloud_out = PointCloud2()
         
-        self.tcp_scanner_offset = [0.0,0.0,0.0]
+        
         
         
         rospy.loginfo("Transform publisher running")
@@ -42,13 +44,20 @@ class KeyenceTransformNode():
         t.header.frame_id = "mur620c/UR10_r/base_link"
         t.child_frame_id = "sensor_optical_frame"
 
+        R = tf_conversions.transformations.quaternion_matrix([data.pose.orientation.x,data.pose.orientation.y,data.pose.orientation.z,data.pose.orientation.w])
+
         t.transform.translation.x = -data.pose.position.x
         t.transform.translation.y = -data.pose.position.y
-        t.transform.translation.z = data.pose.position.z
-        t.transform.rotation.x = data.pose.orientation.x
-        t.transform.rotation.y = data.pose.orientation.y
-        t.transform.rotation.z = data.pose.orientation.z
-        t.transform.rotation.w = data.pose.orientation.w
+        t.transform.translation.z = data.pose.position.z - self.tcp_scanner_offset[2]
+        
+        # the data is rotated by 180 degrees around the z axis
+        q = tf_conversions.transformations.quaternion_from_euler(math.pi,0,-math.pi/4)
+        q_transform = tf_conversions.transformations.quaternion_multiply(q,[data.pose.orientation.x,data.pose.orientation.y,data.pose.orientation.z,data.pose.orientation.w])
+        
+        t.transform.rotation.x = q_transform[0]
+        t.transform.rotation.y = q_transform[1]
+        t.transform.rotation.z = q_transform[2]
+        t.transform.rotation.w = q_transform[3]
         self.transform = t
 
         br.sendTransform(t)
